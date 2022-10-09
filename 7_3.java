4e4a6aa7dc7b9a634ddc5d66aa651f1bd601a363
Kaivanshah1 <110343220+Kaivanshah1@users.noreply.github.com>
2022-10-09 18:49:42 +0530

    Create 7_3.java

---
+++ 7_3.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class AccountHolder {
+    int age, monthlyIncome, workExperience;
+    String name, citizenship;
+
+    AccountHolder(int age, int monthlyIncome, int workExperience, String name, String citizenship) {
+        this.age = age;
+        this.monthlyIncome = monthlyIncome;
+        this.workExperience = workExperience;
+        this.name = name;
+        this.citizenship = citizenship;
+    }
+
+    boolean checkEligibility() {
+        if ((age >= 21 && age <= 60) && (monthlyIncome >= 15000) && (workExperience >= 1) && (citizenship == "Indian")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+public class Practical7_3 {
+    public static void main(String[] args) {
+        AccountHolder a1 = new AccountHolder(17, 16000, 1, "AccountHolder0", "Indian");
+        AccountHolder a2 = new AccountHolder(22, 16000, 2, "AccountHolder1", "Indian");
+        AccountHolder a3 = new AccountHolder(21, 20000, 1, "AccountHolder2", "Kazakistan");
+        AccountHolder a4 = new AccountHolder(25, 25000, 0, "AccountHolder3", "Morocco");
+        AccountHolder a5 = new AccountHolder(65, 20000, 1, "AccountHolder4", "Indian");
+        ArrayList<AccountHolder> arrayList = new ArrayList<>();
+        arrayList.add(a1);
+        arrayList.add(a2);
+        arrayList.add(a3);
+        arrayList.add(a4);
+        arrayList.add(a5);
+        for (int i = 0; i < 5; i++) {
+            if (arrayList.get(i).checkEligibility()) {
+                System.out.println("AccountHolder" + i + " is eligible for personal loan");
+            } else {
+                System.out.println("AccountHolder" + i + " is not eligible for personal loan");
+            }
+        }
+        
+    }
+}
