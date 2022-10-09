a1a21ae18537af258283b6af4ca90b60ac0e2193
Kaivanshah1 <110343220+Kaivanshah1@users.noreply.github.com>
2022-10-09 18:47:53 +0530

    Create 7_2.java

---
+++ 7_2.java
@@ -0,0 +1,31 @@
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Practical7_2 {
+
+    public static void main(String[] args) {
+        String text = "Hello how do you do " + "Have  a good  Day.  Have  fun!";
+        System.out.println(text);
+        Map<String, Integer> map = new TreeMap<>();
+        String[] words = text.split("[  \n\t\r.,;:!?()]");
+        for (int i = 0; i < words.length; i++) {
+            String key = words[i].toLowerCase();
+            if (key.length() > 0) {
+                if (!map.containsKey(key)) {
+                    map.put(key, 1);
+                } else {
+                    int value = map.get(key);
+                    value++;
+                    map.put(key, value);
+                }
+            }
+        }
+        Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+        for (Map.Entry<String, Integer> entry : entrySet) {
+            System.out.println(entry.getKey() + "--> " + entry.getValue());
+        }
+        System.out.println(map);
+    }
+
+}
